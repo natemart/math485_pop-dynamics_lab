0f0756456c159935f21ca507da8a1e55f1db385e
natemart <natemrtn@arizona.edu>
2023-04-15 09:53:23 -0700

    first draft of code

--- age_groups/age_groups.py
+++ age_groups/age_groups.py
@@ -2,33 +2,86 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-cenPop = { 
-    '2010' : pd.read_csv('./2010.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2011' : pd.read_csv('./2011.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2012' : pd.read_csv('./2012.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2013' : pd.read_csv('./2013.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2014' : pd.read_csv('./2014.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2015' : pd.read_csv('./2015.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2016' : pd.read_csv('./2016.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2017' : pd.read_csv('./2017.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2018' : pd.read_csv('./2018.csv', header=0, skiprows=[1], skip_blank_lines=True),
-    '2019' : pd.read_csv('./2019.csv', header=0, skiprows=[1], skip_blank_lines=True)
+# load data
+cenPop = {
+    2010 : pd.read_csv('./2010.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2011 : pd.read_csv('./2011.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2012 : pd.read_csv('./2012.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2013 : pd.read_csv('./2013.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2014 : pd.read_csv('./2014.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2015 : pd.read_csv('./2015.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2016 : pd.read_csv('./2016.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2017 : pd.read_csv('./2017.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2018 : pd.read_csv('./2018.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float}),
+    2019 : pd.read_csv('./2019.csv', header=0, skiprows=[1], skip_blank_lines=True, dtype={'Age':str, 'Both sexes':float, 'Both sexes.1': float, 'Male':float, 'Male.1':float, 'Female':float, 'Female.1':float})
     }
 
 
-cen2010_0_14 = cenPop['2010'].iloc[2:5, :]
-cen2010_15_44 = cenPop['2010'].iloc[5:11, :]
-cen2010_45_over = cenPop['2010'].iloc[11:, :]
+main_data = {yr : (cenPop[yr].iloc[1:4, :]['Both sexes'].sum(), cenPop[yr].iloc[4:10, :]['Both sexes'].sum(), cenPop[yr].iloc[10:19, :]['Both sexes'].sum()) for yr in cenPop}
+
+
+
+
+# group1: 0 - 14yrs, group2: 15 - 44 yrs, group3: 44+ yrs
+grp1 = cenPop[2010].iloc[1:4, :]
+grp2 = cenPop[2010].iloc[4:10, :]
+grp3 = cenPop[2010].iloc[10:19, :]
 
 # general fertility rate 2010 (gfr) per 1000 women ages 15 - 44
-b = 64.0
+gfr2010 = 64.0 / 1000
+
+# 2010 estimated birth rate
+birthRate2010 = (gfr2010 * grp2['Female'].sum()) / grp2['Both sexes'].sum()
 
 # death rates per 100,000 people
-d0_14= 588.0 + 24.0 + 11.5 + 14.0
-d15_44 = 45.5 + 83.8 + 99.7 + 117.3 + 147.2 + 202.4
-d45_over = 377.3 + 491.3 + 730.6 + 1032.2 + 1454.0 + 2246.1 + 3560.5 + 5944.6 + 13407.9
+deathRate_grp1= (588.0 + 24.0 + 11.5 + 14.0) / 100000
+deathRate_grp2 = (45.5 + 83.8 + 99.7 + 117.3 + 147.2 + 202.4) / 100000
+deathRate_grp3 = (311.3 + 491.3 + 730.6 + 1032.2 + 1454.0 + 2246.1 + 3560.5 + 5944.6 + 13407.9) / 100000
 
-# splitting 2010 census into age groups
+# probability of movement of group1 to group2, and from group2 - group3
+probGrp1_2 = 14 / grp1['Both sexes'].sum()
+probGrp2_3 = (44 - 15) / grp2['Both sexes'].sum()
+
+estimated_data = {2010: (grp1['Both sexes'].sum(), grp2['Both sexes'].sum(), grp3['Both sexes'].sum())} 
+for i in cenPop:
+    if i + 1 <= 2019:
+        grp1 = estimated_data[i][0] + birthRate2010*estimated_data[i][1] - deathRate_grp1*estimated_data[i][0] - probGrp1_2*estimated_data[i][0]
+        grp2 = estimated_data[i][1] + probGrp1_2*estimated_data[i][0] - deathRate_grp2*estimated_data[i][1] - probGrp2_3*estimated_data[i][1]
+        grp3 = estimated_data[i][2] + probGrp2_3*estimated_data[i][1] - deathRate_grp3*estimated_data[i][2] 
+        estimated_data[i + 1] = (grp1, grp2, grp3)
+    else:
+        break
+
+comparison = {
+    'main_grp1' : [main_data[yr][0] for yr in main_data],
+    'est_grp1' : [estimated_data[yr][0] for yr in estimated_data],
+    'main_grp2' : [main_data[yr][1] for yr in main_data],
+    'est_grp2': [estimated_data[yr][1] for yr in estimated_data],
+    'main_grp3' : [main_data[yr][2] for yr in main_data],
+    'est_grp3' : [estimated_data[yr][2] for yr in estimated_data]
+    }
+    
+x = np.arange(len(main_data))
+width= 0.1
+multiplier = 0
+fig, ax = plt.subplots(layout='constrained')
+for yr, groups in comparison.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, groups, width, label=yr)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+
+ax.set_ylabel('population size')
+ax.set_xticks(x + width, np.arange(2010, 2020))
+ax.legend(loc='upper left', ncols=6)
+
+plt.show()
+
+
+
+
+
 
 
 
